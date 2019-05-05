2ad9f4693b1016e92355361f45c28776bbef3ade
Dominik Lindorfer <dominik.lindorfer@jku.at>
2019-05-05 17:05:06 +0200

    Added Support for new H264 Player

--- MoodleDownload.sh
+++ MoodleDownload.sh
@@ -1,6 +1,7 @@
 url=$1
 outname=$2
 
-ffmpeg -i "$url" -c copy -bsf:a aac_adtstoasc "$outname"
+#ffmpeg -i "$url" -c copy -bsf:a aac_adtstoasc "$outname"
+ffmpeg -i "$url" -c copy -bsf:v h264_mp4toannexb "$outname"
 
 #Adapted from https://stackoverflow.com/questions/32528595/ffmpeg-mp4-from-http-live-streaming-m3u8-file
