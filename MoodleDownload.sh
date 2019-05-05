86b33cc28f3a8d9e62f7a4839d4e1a8f4bb2e875
Dominik Lindorfer <dominik.lindorfer@jku.at>
2019-05-05 17:18:19 +0200

    Added the option to let ffmpeg choose the conversion based on the output format

--- MoodleDownload.sh
+++ MoodleDownload.sh
@@ -4,4 +4,6 @@ outname=$2
 #ffmpeg -i "$url" -c copy -bsf:a aac_adtstoasc "$outname"
 ffmpeg -i "$url" -c copy -bsf:v h264_mp4toannexb "$outname"
 
+#ffmpeg -i "$url" -c:a copy "$outname"
+
 #Adapted from https://stackoverflow.com/questions/32528595/ffmpeg-mp4-from-http-live-streaming-m3u8-file
